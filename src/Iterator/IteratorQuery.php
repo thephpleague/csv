666f3054719b799d887f54270429743aeb70ba76
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2014-04-15 13:32:05 +0200

    Bump version

--- src/Iterator/IteratorQuery.php
+++ src/Iterator/IteratorQuery.php
@@ -6,7 +6,7 @@
 * @copyright 2014 Ignace Nyamagana Butera
 * @link https://github.com/thephpleague/csv/
 * @license http://opensource.org/licenses/MIT
-* @version 5.3.1
+* @version 5.4.0
 * @package League.csv
 *
 * MIT LICENSE
