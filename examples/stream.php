832097578152a2828c76f9fad705bb6f45943ed0
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2014-04-28 14:51:47 +0200

    update stream example

--- examples/stream.php
+++ examples/stream.php
@@ -16,9 +16,9 @@ require 'lib/FilterTranscode.php';
 
 stream_filter_register(FilterTranscode::FILTER_NAME."*", "\lib\FilterTranscode");
 $reader = new Reader('data/prenoms.csv');
-$reader->addStreamFilter(FilterTranscode::FILTER_NAME."iso-8859-1:utf-8");
-$reader->addStreamFilter('string.toupper');
-$reader->addStreamFilter('string.rot13');
+$reader->appendStreamFilter(FilterTranscode::FILTER_NAME."iso-8859-1:utf-8");
+$reader->appendStreamFilter('string.toupper');
+$reader->appendStreamFilter('string.rot13');
 $reader->setDelimiter(';');
 $reader->setOffset(6);
 $reader->setLimit(3);
@@ -47,18 +47,18 @@ var_dump($res);
 // You must instantiate a new Class for each changes
 touch('/tmp/test.csv');
 $writer = new Writer(new SplFileInfo('/tmp/test.csv'), 'w');
-$writer->addStreamFilter('string.toupper');
+$writer->appendStreamFilter('string.toupper');
 $writer->insertOne('je,suis,toto,le,héros');
-$writer->addStreamFilter('string.rot13');
+$writer->appendStreamFilter('string.rot13');
 $writer->insertOne('je,suis,toto,le,héros');
 /*
 the data is :
  - uppercased only
 */
 $writer = new Writer('/tmp/test.csv', 'a+');
-$writer->addStreamFilter('string.toupper');
-$writer->addStreamFilter(FilterTranscode::FILTER_NAME."iso-8859-1:utf-8");
-$writer->addStreamFilter('string.rot13');
+$writer->appendStreamFilter('string.toupper');
+$writer->appendStreamFilter(FilterTranscode::FILTER_NAME."iso-8859-1:utf-8");
+$writer->appendStreamFilter('string.rot13');
 $writer->removeStreamFilter(FilterTranscode::FILTER_NAME."iso-8859-1:utf-8");
 $writer->insertOne('je,suis,toto,le,héros');
 /*
