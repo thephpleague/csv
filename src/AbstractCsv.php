b99e10d043675c17f6b92b7d09ef8a9a32b927fe
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2014-08-26 12:31:51 +0200

    remove Formatter trait

--- src/AbstractCsv.php
+++ src/AbstractCsv.php
@@ -12,15 +12,17 @@
 */
 namespace League\Csv;
 
+use DomDocument;
 use InvalidArgumentException;
 use IteratorAggregate;
 use JsonSerializable;
 use League\Csv\Config\Controls;
-use League\Csv\Config\Formatter;
 use League\Csv\Config\StreamFilter;
+use League\Csv\Iterator\MapIterator;
 use SplFileInfo;
 use SplFileObject;
 use SplTempFileObject;
+use Traversable;
 
 /**
  *  An abstract class to enable basic CSV manipulation
@@ -35,11 +37,6 @@ abstract class AbstractCsv implements JsonSerializable, IteratorAggregate
      *  Csv Controls Trait
      */
     use Controls;
-
-    /**
-     *  Csv Formatter Trait
-     */
-    use Formatter;
 
     /**
      *  Stream Filter API Trait
@@ -61,6 +58,13 @@ abstract class AbstractCsv implements JsonSerializable, IteratorAggregate
      * @var string
      */
     protected $open_mode;
+
+    /**
+     * Charset Encoding for the CSV
+     *
+     * @var string
+     */
+    protected $encodingFrom = 'UTF-8';
 
     /**
      * The constructor
@@ -273,6 +277,59 @@ abstract class AbstractCsv implements JsonSerializable, IteratorAggregate
     }
 
     /**
+     * Set the CSV encoding charset
+     *
+     * @param string $str
+     *
+     * @return $this
+     */
+    public function setEncodingFrom($str)
+    {
+        $str = str_replace('_', '-', $str);
+        $str = filter_var($str, FILTER_SANITIZE_STRING, ['flags' => FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH]);
+        if (empty($str)) {
+            throw new InvalidArgumentException('you should use a valid charset');
+        }
+        $this->encodingFrom = strtoupper($str);
+
+        return $this;
+    }
+
+    /**
+     * Get the source CSV encoding charset
+     *
+     * @return string
+     */
+    public function getEncodingFrom()
+    {
+        return $this->encodingFrom;
+    }
+
+    /**
+     * Output all data on the CSV file
+     *
+     * @param string $filename CSV downloaded name if present adds extra headers
+     */
+    public function output($filename = null)
+    {
+        $iterator = $this->getIterator();
+        //@codeCoverageIgnoreStart
+        if (! is_null($filename) && self::isValidString($filename)) {
+            $filename = (string) $filename;
+            $filename = filter_var($filename, FILTER_UNSAFE_RAW, ['flags' => FILTER_FLAG_STRIP_LOW]);
+            header('Content-Type: application/octet-stream');
+            header('Content-Transfer-Encoding: binary');
+            header('Content-Disposition: attachment; filename="'.$filename.'"');
+            if (! $iterator instanceof SplTempFileObject) {
+                header('Content-Length: '.$iterator->getSize());
+            }
+        }
+        //@codeCoverageIgnoreEnd
+        $iterator->rewind();
+        $iterator->fpassthru();
+    }
+
+    /**
     * Validate a variable to be stringable
     *
     * @param mixed $str
@@ -282,5 +339,83 @@ abstract class AbstractCsv implements JsonSerializable, IteratorAggregate
     public static function isValidString($str)
     {
         return is_scalar($str) || (is_object($str) && method_exists($str, '__toString'));
+    }
+
+    /**
+     * Retrieves the CSV content
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        ob_start();
+        $this->output();
+
+        return ob_get_clean();
+    }
+
+    /**
+     * Return a HTML table representation of the CSV Table
+     *
+     * @param string $class_name optional classname
+     *
+     * @return string
+     */
+    public function toHTML($class_name = 'table-csv-data')
+    {
+        $doc = $this->toXML('table', 'tr', 'td');
+        $doc->documentElement->setAttribute('class', $class_name);
+
+        return $doc->saveHTML($doc->documentElement);
+    }
+
+    /**
+     * transform a CSV into a XML
+     *
+     * @param string $root_name XML root node name
+     * @param string $row_name  XML row node name
+     * @param string $cell_name XML cell node name
+     *
+     * @return \DomDocument
+     */
+    public function toXML($root_name = 'csv', $row_name = 'row', $cell_name = 'cell')
+    {
+        $doc = new DomDocument('1.0', 'UTF-8');
+        $root = $doc->createElement($root_name);
+        $iterator = $this->convertToUtf8($this->getIterator());
+        foreach ($iterator as $row) {
+            $item = $doc->createElement($row_name);
+            foreach ($row as $value) {
+                $content = $doc->createTextNode($value);
+                $cell = $doc->createElement($cell_name);
+                $cell->appendChild($content);
+                $item->appendChild($cell);
+            }
+            $root->appendChild($item);
+        }
+        $doc->appendChild($root);
+
+        return $doc;
+    }
+
+    /**
+     * Convert Csv file into UTF-8
+     *
+     * @return \Traversable
+     */
+    protected function convertToUtf8(Traversable $iterator)
+    {
+        if (strpos($this->encodingFrom, 'UTF-8') !== false) {
+            return $iterator;
+        }
+
+        return new MapIterator($iterator, function ($row) {
+            foreach ($row as &$value) {
+                $value = mb_convert_encoding($value, 'UTF-8', $this->encodingFrom);
+            }
+            unset($value);
+
+            return $row;
+        });
     }
 }
