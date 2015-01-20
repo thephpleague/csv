7333dc1176a2c6c795282dec4a93b3e6a4810f58
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2015-01-20 12:43:03 +0100

    bug fix output method

--- src/Config/Output.php
+++ src/Config/Output.php
@@ -130,7 +130,7 @@ trait Output
             $fname = filter_var($fname, FILTER_UNSAFE_RAW, ['flags' => FILTER_FLAG_STRIP_LOW]);
             header('Content-Type: application/octet-stream');
             header('Content-Transfer-Encoding: binary');
-            header("Content-Disposition: attachment; filename=\"$fname\"; filename*=UTF-8' '".rawurlencode($fname));
+            header("Content-Disposition: attachment; filename=\"$fname\"; filename*=UTF-8 ".rawurlencode($fname));
         }
         //@codeCoverageIgnoreEnd
         echo $this->bom;
