6e007833cffe199d3d9bfc2333bf165e3a4ca4d3
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2017-10-16 13:43:29 +0200

    Adding EscapeFormulaInjection formatter

---
+++ src/EscapeFormulaInjection.php
@@ -0,0 +1,148 @@
+<?php
+/**
+* This file is part of the League.csv library
+*
+* @license http://opensource.org/licenses/MIT
+* @link https://github.com/thephpleague/csv/
+* @version 9.1.0
+* @package League.csv
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+declare(strict_types=1);
+
+namespace League\Csv;
+
+/**
+ * A League CSV formatter to tackle CSV Formula Injection
+ *
+ * @see http://georgemauer.net/2017/10/07/csv-injection.html
+ *
+ * @package League.csv
+ * @since   9.1.0
+ * @author  Ignace Nyamagana Butera <nyamsprod@gmail.com>
+ */
+class EscapeFormulaInjection
+{
+    /**
+     * Special characters that will be escaped
+     *
+     * @var string[]
+     */
+    protected $special_chars = [];
+
+    /**
+     * Escape character
+     *
+     * @var string
+     */
+    protected $escape;
+
+    /**
+     * New instance
+     *
+     * @param string   $escape
+     * @param string[] $special_chars additional special characters to escape
+     */
+    public function __construct(string $escape = "\t", array $special_chars = [])
+    {
+        $this->escape = $escape;
+        if (!empty($special_chars)) {
+            $special_chars = $this->filterSpecialCharacters(...$special_chars);
+        }
+        $special_chars = array_merge(['=', '-', '+', '@'], $special_chars);
+        $this->special_chars = array_fill_keys(array_unique($special_chars), 1);
+    }
+
+    /**
+     * Filter submitted special characters.
+     *
+     * @param string ...$characters
+     *
+     * @return string[]
+     */
+    protected function filterSpecialCharacters(string ...$characters): array
+    {
+        return $characters;
+    }
+
+    /**
+     * Returns the list of character the instance will escape.
+     *
+     * @return string[]
+     */
+    public function getSpecialCharacters(): array
+    {
+        return array_keys($this->special_chars);
+    }
+
+    /**
+     * Returns the escape character.
+     *
+     * @return string
+     */
+    public function getEscape(): string
+    {
+        return $this->escape;
+    }
+
+    /**
+     * League CSV formatter hook.
+     *
+     * @see escapeRecord
+     *
+     * @param array $record
+     *
+     * @return array
+     */
+    public function __invoke(array $record): array
+    {
+        return $this->escapeRecord($record);
+    }
+
+    /**
+     * Escape a CSV record.
+     *
+     * @param array $record
+     *
+     * @return array
+     */
+    public function escapeRecord(array $record): array
+    {
+        return array_map([$this, 'escapeField'], $record);
+    }
+
+    /**
+     * Escape a CSV cell.
+     *
+     * @param mixed $cell
+     *
+     * @return mixed
+     */
+    protected function escapeField($cell)
+    {
+        if (!$this->isStringable($cell)) {
+            return $cell;
+        }
+
+        $str_cell = (string) $cell;
+        if (isset($str_cell[0], $this->special_chars[$str_cell[0]])) {
+            return $this->escape.$str_cell;
+        }
+
+        return $cell;
+    }
+
+    /**
+     * Tell whether the submitted value is stringable.
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    protected function isStringable($value): bool
+    {
+        return is_string($value) || (is_object($value) && method_exists($value, '__toString'));
+    }
+}
---
+++ tests/EscapeFormulaInjectionTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace LeagueTest\Csv;
+
+use League\Csv\EscapeFormulaInjection;
+use League\Csv\Writer;
+use PHPUnit\Framework\TestCase;
+use SplTempFileObject;
+use TypeError;
+
+/**
+ * @group filter
+ * @coversDefaultClass League\Csv\EscapeFormulaInjection
+ */
+class EscapeFormulaInjectionTest extends TestCase
+{
+    /**
+     * @covers ::__construct
+     * @covers ::filterSpecialCharacters
+     */
+    public function testConstructorThrowsTypError()
+    {
+        $this->expectException(TypeError::class);
+        new EscapeFormulaInjection("\t", [(object) 'i']);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getEscape
+     */
+    public function testGetEscape()
+    {
+        $formatter = new EscapeFormulaInjection();
+        $this->assertSame("\t", $formatter->getEscape());
+        $formatterBis = new EscapeFormulaInjection("\n");
+        $this->assertSame("\n", $formatterBis->getEscape());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getSpecialCharacters
+     * @covers ::filterSpecialCharacters
+     */
+    public function testGetSpecialChars()
+    {
+        $formatter = new EscapeFormulaInjection();
+        $this->assertNotContains('i', $formatter->getSpecialCharacters());
+        $formatterBis = new EscapeFormulaInjection("\t", ['i']);
+        $this->assertContains('i', $formatterBis->getSpecialCharacters());
+    }
+
+    /**
+     * @covers ::escapeRecord
+     * @covers ::escapeField
+     * @covers ::isStringable
+     */
+    public function testEscapeRecord()
+    {
+        $record = ['2', '2017-07-25', 'Important Client', '=2+5', 240, null, (object) 'yes'];
+        $expected = ['2', '2017-07-25', 'Important Client', "\t=2+5", 240, null, (object) 'yes'];
+        $formatter = new EscapeFormulaInjection();
+        $this->assertEquals($expected, $formatter->escapeRecord($record));
+    }
+
+    /**
+     * @covers ::__invoke
+     * @covers ::escapeRecord
+     * @covers ::escapeField
+     * @covers ::isStringable
+     */
+    public function testFormatterOnWriter()
+    {
+        $record = ['2', '2017-07-25', 'Important Client', '=2+5', 240, null];
+        $expected = "2,2017-07-25,\"Important Client\",\"\t=2+5\",240,\n";
+        $csv = Writer::createFromFileObject(new SplTempFileObject());
+        $csv->addFormatter(new EscapeFormulaInjection());
+        $csv->insertOne($record);
+        $this->assertContains($expected, $csv->getContent());
+    }
+}
