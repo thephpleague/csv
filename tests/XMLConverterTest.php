0f0f007044d96a7ecee06a9ef27e27fc6f16ab8b
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2018-09-21 15:04:31 +0200

    prepare 9.2.0 release

--- tests/XMLConverterTest.php
+++ tests/XMLConverterTest.php
@@ -5,7 +5,7 @@
  *
  * @author  Ignace Nyamagana Butera <nyamsprod@gmail.com>
  * @license https://github.com/thephpleague/csv/blob/master/LICENSE (MIT License)
- * @version 9.1.5
+ * @version 9.2.0
  * @link    https://github.com/thephpleague/csv
  *
  * For the full copyright and license information, please view the LICENSE
