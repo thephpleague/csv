da17e259f4c09a417b1987b533368c210aa43f73
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2018-10-06 20:54:26 +0200

    update documentation website layout

--- docs/custom.js
+++ docs/custom.js
@@ -1,23 +1,17 @@
 (() => {
   const uri = new URL(location.href);
 
-  document.querySelector('.versions').addEventListener('change', function() {
-    uri.hash = '';
-    let path = this.options[this.options.selectedIndex].dataset.url;
-    if (undefined === path) {
-      return;
-    }
-    uri.pathname = path;
-    location.href = uri.toString();
+  document.querySelector('.tool nav h2').addEventListener('click', function () {
+    this.parentNode.querySelector('ul').classList.toggle('show-nav');
   }, false);
 
-  document.querySelectorAll("main h2[id]").forEach((heading) => {
-    uri.hash = heading.id;
+  document.querySelectorAll("main h2[id]").forEach((header) => {
+    uri.hash = header.id;
     let link = document.createElement("a");
     link.className = "header-permalink";
     link.title = "Permalink";
     link.href = uri.toString();
     link.innerHTML = "&#182;";
-    heading.appendChild(link);
+    header.appendChild(link);
   });
 })();
