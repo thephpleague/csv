ab5f057ceab62f3242c394f718bd883f6d029166
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2020-05-26 07:31:22 +0200

    Improve sponsorship banner

--- docs/custom.js
+++ docs/custom.js
@@ -1,19 +1,19 @@
 (() => {
-  const uri = new URL(location.href);
 
   let headers = document.querySelector('header nav h2');
   if (headers) {
-    headers.addEventListener('click', function () {
+    headers.addEventListener('click', ()  => {
       this.parentNode.querySelector('ul').classList.toggle('show');
     }, false);
   }
 
   let contentHeaders = document.querySelectorAll("main h2[id]");
-  if (contentHeaders && !document.querySelector('html').classList.contains('homepage')) {
+  if (!document.querySelector('html').classList.contains('homepage') && contentHeaders) {
+    const uri = new URL(location.href);
     contentHeaders.forEach((header) => {
       uri.hash = header.id;
       let link = document.createElement("a");
-      link.className = "header-permalink";
+      link.classList.add("header-permalink");
       link.title = "Permalink";
       link.href = uri.toString();
       link.innerHTML = "&#182;";
@@ -21,16 +21,22 @@
     });
   }
 
-  const sponsorDiv = document.querySelector('.sponsors');
-  if (sponsorDiv) {
-    let hideSponsorUntil = localStorage.getItem('hideSponsorUntil');
-    if (hideSponsorUntil === null || hideSponsorUntil < (new Date().getTime())) {
-      sponsorDiv.classList.remove('hide');
+  const sponsorBanner = document.querySelector('.sponsors');
+  if (sponsorBanner) {
+    let hideUntil = localStorage.getItem('hideSponsorUntil');
+    if (hideUntil === null || hideUntil < (new Date().getTime())) {
+      localStorage.removeItem('hideSponsorUntil');
+      sponsorBanner.classList.remove('hide');
     }
 
-    sponsorDiv.querySelector('a.close').addEventListener('click', function () {
-      sponsorDiv.classList.add('hide');
+    let closeButton = document.createElement('a');
+    closeButton.classList.add('close');
+    closeButton.innerHTML = 'close me';
+    closeButton.addEventListener('click',  () => {
       localStorage.setItem('hideSponsorUntil', new Date().getTime() + (7 * 86400 * 1e4));
+      sponsorBanner.classList.add('hide');
     }, false);
+
+    sponsorBanner.firstElementChild.appendChild(closeButton);
   }
 })();
