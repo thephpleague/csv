03be1caee4ccc93c2c09147387e81e7261edc76a
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2021-02-21 21:27:35 +0100

    internal usage of array_map

--- src/EncloseField.php
+++ src/EncloseField.php
@@ -15,6 +15,7 @@ namespace League\Csv;
 
 use InvalidArgumentException;
 use php_user_filter;
+use function array_map;
 use function in_array;
 use function str_replace;
 use function strcspn;
@@ -92,13 +93,10 @@ class EncloseField extends php_user_filter
             throw new InvalidArgumentException('The sequence must contain at least one character to force enclosure');
         }
 
-        $formatter = static function (array $record) use ($sequence): array {
-            foreach ($record as &$value) {
-                $value = $sequence.$value;
-            }
-            unset($value);
-
-            return $record;
+        $formatter = function (array $record) use ($sequence): array {
+            return array_map(function (?string $value) use ($sequence): string {
+                return $sequence.$value;
+            }, $record);
         };
 
         return $csv
