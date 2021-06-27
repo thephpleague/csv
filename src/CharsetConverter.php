6fe3c30c94a045a8e05747dcf76c43692c87225e
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2021-06-27 09:34:45 +0200

    Update package development requirement

--- src/CharsetConverter.php
+++ src/CharsetConverter.php
@@ -39,18 +39,6 @@ class CharsetConverter extends php_user_filter
     const FILTERNAME = 'convert.league.csv';
 
     /**
-     * the filter name used to instantiate the class with.
-     *
-     * @var string
-     */
-    public $filtername;
-
-    /**
-     * @var mixed value passed to passed to stream_filter_append or stream_filter_prepend functions.
-     */
-    public $params;
-
-    /**
      * The records input encoding charset.
      *
      * @var string
--- src/RFC4180Field.php
+++ src/RFC4180Field.php
@@ -43,18 +43,6 @@ class RFC4180Field extends php_user_filter
     public const FILTERNAME = 'convert.league.csv.rfc4180';
 
     /**
-     * the filter name used to instantiate the class with.
-     *
-     * @var string
-     */
-    public $filtername;
-
-    /**
-     * @var mixed value passed to passed to stream_filter_append or stream_filter_prepend functions.
-     */
-    public $params;
-
-    /**
      * The value being search for.
      *
      * @var string[]
